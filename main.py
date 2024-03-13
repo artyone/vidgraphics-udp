da56e6ebe92a24a31d11f0fdefcf86976c49b84d
Arty <gizzmaster@gmail.com>
2024-03-13 16:27:27 +0300

    fix close bug. add presets

--- main.py
+++ main.py
@@ -1,63 +1,71 @@
 import os
 import sys
 import time
-from itertools import cycle
-from itertools import count
+from functools import partial
+from itertools import count, cycle
 
 import numpy as np
 import pyqtgraph as pg
-from PyQt5.QtCore import Qt, QThread, QTimer, pyqtSignal
-from PyQt5.QtGui import QPixmap, QColor, QFont, QPalette, QIcon
+from PyQt5.QtCore import QSettings, Qt, QThread, QTimer, pyqtSignal
+from PyQt5.QtGui import QColor, QFont, QIcon, QPalette, QPixmap
 from PyQt5.QtNetwork import QUdpSocket
 from PyQt5.QtWidgets import (QAction, QApplication, QComboBox, QHBoxLayout,
-                             QLabel, QMainWindow, QMenu, QMessageBox,
-                             QPushButton, QSlider, QStyle, QToolBar, QSplitter,
-                             QTreeWidget, QTreeWidgetItem, QVBoxLayout,
-                             QWidget)
-from scipy import signal
-import qdarktheme
+                             QInputDialog, QLabel, QMainWindow, QMenu,
+                             QMessageBox, QPushButton, QSlider, QSplitter,
+                             QStyle, QToolBar, QTreeWidget, QTreeWidgetItem,
+                             QVBoxLayout, QWidget)
 
-      
 
 class MainData:
     categories = {
         'main': {
             'headers': [
-                'MD', 'curr_27V', 'u_36V_C', 'u_36V_A', 'u_36V_B', 
-                'u15V_p_AP', 'u15V_m_AP', 'u27V_del', 'alfa', 'u_5V', 
-                'EA', 'EH','current', 'signal_D','Unn', 
+                'MD', 'curr_27V', 'u_36V_C', 'u_36V_A', 'u_36V_B',
+                'u15V_p_AP', 'u15V_m_AP', 'u27V_del', 'alfa', 'u_5V',
+                'EA', 'EH', 'current', 'signal_D', 'Unn',
                 'Una', 'D_analog', 'gamma', 'epsilon', 'psi',
-                'ARU','E_H_ap','E_g','E_v','E_A_ap',
-                'u_12V','u_12V_gnd','u_12V_m_018A','u_12V_m_018A_gnd','u_48V',
-                'u_48V_gnd','u_8V', 'u_8V_gnd','u_6V_m_0075A','u_6V_m_gnd',
-                'u_12V_0075A','u_12V_0075A_gnd', 'u_6V','u_6V_gnd','u_6V_m_028A',
-                'u_6V_m_028A_gnd'
+                'ARU', 'E_H_ap', 'E_g', 'E_v', 'E_A_ap',
+                'u_12V', 'u_12V_gnd', 'u_12V_m_018A', 'u_12V_m_018A_gnd', 'u_48V',
+                'u_48V_gnd', 'u_8V', 'u_8V_gnd', 'u_6V_m_0075A', 'u_6V_m_gnd',
+                'u_12V_0075A', 'u_12V_0075A_gnd', 'u_6V', 'u_6V_gnd', 'u_6V_m_028A',
+                'u_6V_m_028A_gnd', 'zad_izc'
             ],
-            'coef': [ 
-                0.01, 1, 1, 1, 1, 
-                1, 1, 1, 1, 1, 
-                0.00244, 0.00244, 1, 0.00488, 1, 
-                1, 0.00488, 0.00244, 0.00244, 0.00244, 
-                1, 0.1, 1, 1, 0.1, 
-                1, 1, 1, 1, 1, 
-                1, 1, 1, 1, 1, 
-                1, 1, 1, 1, 1, 
-                1, 
-            ], 
+            'coef': [
+                0.01, 1, 1, 1, 1,
+                1, 1, 1, 1, 1,
+                0.00244, 0.00244, 1, 0.00488, 1,
+                1, 0.00488, 0.00244, 0.00244, 0.00244,
+                1, 0.1, 1, 1, 0.1,
+                1, 1, 1, 1, 1,
+                1, 1, 1, 1, 1,
+                1, 1, 1, 1, 1,
+                1, 0.01,
+            ],
+            'types': [
+                np.uint16, np.int16, np.int16, np.int16, np.int16,
+                np.int16, np.int16, np.int16, np.int16, np.int16,
+                np.int16, np.int16, np.int16, np.int16, np.int16,
+                np.int16, np.int16, np.int16, np.int16, np.int16,
+                np.int16, np.int16, np.int16, np.int16, np.int16,
+                np.int16, np.int16, np.int16, np.int16, np.int16,
+                np.int16, np.int16, np.int16, np.int16, np.int16,
+                np.int16, np.int16, np.int16, np.int16, np.int16,
+                np.int16, np.uint16
+            ],
             'visible': True
         },
         'vid_data': {
             'headers': [
                 'vid_data'
-            ], 
+            ],
             'visible': False
         },
         'arinc': {
             'headers': [
-                'ARINC_081', 'ARINC_082', 'ARINC_083', 'ARINC_084', 
-                'ARINC_085', 'ARINC_086', 'ARINC_087', 'ARINC_088', 
-                'ARINC_089'	
-            ], 
+                'ARINC_081', 'ARINC_082', 'ARINC_083', 'ARINC_084',
+                'ARINC_085', 'ARINC_086', 'ARINC_087', 'ARINC_088',
+                'ARINC_089'
+            ],
             'visible': True
         },
         'bit_data': {
@@ -70,15 +78,15 @@ class MainData:
                 'komm_PP', 'kom_mem_ASD', 'ASP', 'Tg_RAZI', 'MD_k', 'Tg_ZHO', 'ZH_ZH', 'Si_k',
                 'PPH', 'Sh_P2', 'izp_k', 'izr_k', 'strob_RZ', 'zona_1', 'zona_2', 'rpo',
                 'AR', 'kom_rg_rv', 'sz', 'kom_ASD_k', 'Tg_ZH_Zh', 'kom_vp', 'null_1', 'null_2',
-                'kontrol_27V_m_pit', 'kontrol_27V_m', 'kontrol_27V_p_pit', 'kontrol_27V_p', 
+                'kontrol_27V_m_pit', 'kontrol_27V_m', 'kontrol_27V_p_pit', 'kontrol_27V_p',
                 'kontrol_27V_p_A0', 'kontrol_27V_p_A1', 'kontrol_27V_p_A2', 'kontrol_27V_p_A3'
-            ], 
+            ],
             'visible': True
         },
         'time_src': {
             'headers': [
                 'time_src'
-            ], 
+            ],
             'visible': True
         }
     }
@@ -101,7 +109,7 @@ class MainData:
         value = getattr(self, name)
         value.extend(data)
         # setattr(self, name, value)
-    
+
     def cut_data(self):
         for category in self.categories.values():
             for name in category['headers']:
@@ -123,9 +131,9 @@ class MainData:
             ('arinc_data', np.uint16, 9),
             ('bit_data', np.uint8, 9),
             ('null_bytes2', np.uint8, (1, )),
-            ('data_main', np.int16, 41),
-            ('null_bytes3', np.uint8, 58),
-            ('time_src', np.uint32, (1, )), 
+            ('data_main', np.int16, 42),
+            ('null_bytes3', np.uint8, 56),
+            ('time_src', np.uint32, (1, )),
             ('null_bytes4', np.uint8, 33)
         ])
         dt = dt.newbyteorder('>')
@@ -135,8 +143,9 @@ class MainData:
     def unpack_data(self, res):
         main_headers = self.categories['main']['headers']
         main_coef = self.categories['main']['coef']
-        for index, (name, coef) in enumerate(zip(main_headers, main_coef)):
-            self.add_data(name, res['data_main'][:,index] * coef)
+        main_types = self.categories['main']['types']
+        for index, (name, coef, type) in enumerate(zip(main_headers, main_coef, main_types)):
+            self.add_data(name, res['data_main'][:, index].astype(type) * coef)
         self.add_data('time_src', res['time_src'][:, 0] * 0.002)
         # self.add_data('time_src', [next(counter) / 50_000])
         for index, col in enumerate(self.categories['arinc']['headers']):
@@ -145,25 +154,25 @@ class MainData:
         columns_bits = [
             [
                 'send_ARINC', 'u27_p', 'u27_ground', 'u27_A', 'u27_A1', 'u36B_m', 'u36A_m', 'u15_m'
-            ], 
+            ],
             [
                 'u15v_p', 'u36C_m', 'u15V_bk', 'off_vob', 'off_V', 'off_ASU', 'block_VP', 'off_CU'
-            ], 
+            ],
             [
                 'vkl_rrch', 'PR_27v', 'block_AB', 'bridge27V', 'VPG_27V', 'komm_ASD', 'block_DP', 'sinhro'
-            ], 
+            ],
             [
                 'RIP', 'D5', 'DVA1', 'DVA2', 'DVA3', 'DVA4', 'kom_vn', 'kontr_toka_rzp'
-            ], 
+            ],
             [
                 'kontr_zahv_apch', 'kontr_vn', 'EhV', 'AV', 'PR_U505', 'Zg_27V', 'Kom_No', 'VK'
-            ], 
+            ],
             [
                 'komm_PP', 'kom_mem_ASD', 'ASP', 'Tg_RAZI', 'MD_k', 'Tg_ZHO', 'ZH_ZH', 'Si_k'
             ],
             [
                 'PPH', 'Sh_P2', 'izp_k', 'izr_k', 'strob_RZ', 'zona_1', 'zona_2', 'rpo'
-            ], 
+            ],
             [
                 'AR', 'kom_rg_rv', 'sz', 'kom_ASD_k', 'Tg_ZH_Zh', 'kom_vp', 'null_1', 'null_2'
             ],
@@ -183,14 +192,12 @@ class MainData:
         if counter > 5_000:
             self.cut_data()
 
-
     @staticmethod
     def unpack_bits(columns, data):
         result = {}
         for i, name in enumerate(columns):
             result[name] = ((data & (1 << i)) >> i).astype(np.bool_)
         return result
-
 
 
 class UpdateGrapicsThread(QThread):
@@ -228,6 +235,7 @@ class IndicatorLabel(QLabel):
         pixmap.fill(Qt.GlobalColor.green)
         self.setPixmap(pixmap)
 
+
 class MainWindow(QMainWindow):
     def __init__(self, app):
         super().__init__()
@@ -250,10 +258,11 @@ class MainWindow(QMainWindow):
         self.cache = b''
         self.update_data_threads = UpdateDataThread()
         self.update_data_threads.update_signal.connect(self.update_data)
+        self.settings = QSettings('settings.ini', QSettings.IniFormat)
         self.initUI()
 
     def initUI(self):
-        self.setWindowTitle("VID GRAPH UPD v.2024.02.07")
+        self.setWindowTitle("VID GRAPH UPD v.2024.03.13")
         self.setGeometry(0, 0, 1350, 768)
 
         main_widget = QWidget()
@@ -264,11 +273,12 @@ class MainWindow(QMainWindow):
 
         self.left_widget = LeftMenuTree(self)
 
-        right_widget = QWidget()
-        right_widget.setStyleSheet('QWidget {background-color: rgb(0, 0, 0)}')
-        right_widget_layout = QVBoxLayout(right_widget)
+        self.right_widget = QWidget()
+        self.right_widget.setStyleSheet(
+            'QWidget {background-color: rgb(0, 0, 0)}')
+        right_widget_layout = QVBoxLayout(self.right_widget)
         right_widget_layout.setContentsMargins(0, 0, 0, 0)
-        
+
         self.right_graph_layout = QVBoxLayout()
         right_widget_layout.addLayout(self.right_graph_layout, 3)
         self.right_graph_layout.setContentsMargins(0, 0, 0, 0)
@@ -280,7 +290,7 @@ class MainWindow(QMainWindow):
         self.right_vid_layout.setSpacing(0)
 
         self.main_layout.addWidget(self.left_widget)
-        self.main_layout.addWidget(right_widget)
+        self.main_layout.addWidget(self.right_widget)
 
         self.set_action()
         self.set_toolbar()
@@ -362,12 +372,67 @@ class MainWindow(QMainWindow):
         button_clear_graph.clicked.connect(self.clear_graphs)
         toolbar.addWidget(button_clear_graph)
 
-        self.addToolBar(pos, toolbar)
+        self.view_menu = QMenu(self)
+        self.view_menu.setTitle('Пресеты графиков')
+        toolbar.addAction(self.view_menu.menuAction())
+        self.update_view_menu()
 
+        self.addToolBar(pos, toolbar)
 
     def clear_graphs(self):
         self.data.clear_data()
         self.update_all_graphics()
+
+    def update_view_menu(self):
+        self.view_menu.clear()
+
+        save_current_action = QAction('Сохранить текущее отображение', self)
+        save_current_action.triggered.connect(self.save_view)
+        self.view_menu.addAction(save_current_action)
+
+        for name, values in self.settings.value('view_settings', {}).items():
+            sub_menu = QMenu(self.view_menu)
+            self.view_menu.addMenu(sub_menu)
+            sub_menu.setTitle(name)
+
+            activate_action = QAction('Восстановить', self)
+            activate_action.triggered.connect(
+                partial(self.restore_view, values))
+            sub_menu.addAction(activate_action)
+
+            delete_action = QAction('Удалить отображение', self)
+            delete_action.triggered.connect(
+                partial(self.delete_view_from_settings, name))
+            sub_menu.addAction(delete_action)
+
+    def save_view(self):
+        names_graph = [a for a in self.graph_widgets.keys()]
+        if not names_graph:
+            return
+
+        text, ok_pressed = QInputDialog.getText(
+            self, 'Введите имя', 'Имя: '
+        )
+        if not ok_pressed or not text:
+            return
+
+        current_values = self.settings.value('view_settings', {})
+        current_values[text] = names_graph
+        self.settings.setValue('view_settings', current_values)
+        self.update_view_menu()
+
+    def restore_view(self, list_names):
+        for name in list(self.graph_widgets):
+            self.delete_graph_window(name)
+        for name in list_names:
+            self.create_graph_window(name)
+
+    def delete_view_from_settings(self, name):
+        current_settings = self.settings.value('view_settings', {})
+        if name in current_settings:
+            current_settings.pop(name)
+            self.settings.setValue('view_settings', current_settings)
+            self.update_view_menu()
 
     def slider_resolution_handler(self):
         self.resolution = self.slider_resolution.value()
@@ -491,7 +556,6 @@ class MainWindow(QMainWindow):
         for name in list(self.graph_widgets.keys()):
             self.delete_graph_window(name)
 
-
     def closeEvent(self, ev):
         if self.process_started:
             self.stop_process()
@@ -505,8 +569,6 @@ class LeftMenuTree(QTreeWidget):
         self.setColumnCount(1)
         self.update_checkbox()
         self.itemDoubleClicked.connect(self.item_double_click_handle)
-
-
 
     def update_checkbox(self):
         self.clear()
@@ -552,16 +614,21 @@ class GraphWidget(pg.PlotWidget):
         self.resolution = 0
         self.curves = {}
         self.getAxis('left').setWidth(50)
+
+        close_action = QAction('Закрыть (Средняя клавиша мышки)')
+        close_action.triggered.connect(
+            lambda: self.main_window.delete_graph_window(self.graph_names))
+        self.scene().contextMenu.append(close_action)
+
         self.create_graphs()
 
     def create_graphs(self):
         self.showGrid(x=True, y=True)
         self.apply_theme('black')
-        self.setMenuEnabled(False)
+        # self.setMenuEnabled(False)
         self.setClipToView(True)
         self.setDownsampling(auto=True, mode='peak')
-        self.scene().sigMouseClicked.connect(self.mouse_click_event)
-        
+
         for name in self.graph_names:
             color = next(self.colors)
             pen = pg.mkPen(color=color, width=2)
@@ -578,6 +645,7 @@ class GraphWidget(pg.PlotWidget):
         #     slot=self.mouse_moved
         # )
 
+        # self.scene().sigMouseClicked.connect(self.mouse_click_event)
         self.scene().sigMouseMoved.connect(self.mouse_moved)
 
     def mouse_moved(self, ev):
@@ -586,10 +654,10 @@ class GraphWidget(pg.PlotWidget):
             self.pos_x = float(mousePoint.x())
             self.pos_y = float(mousePoint.y())
             self.setToolTip(
-                f'<b>{self.pos_y:.3f}</b>' #x: <b>{self.pos_x:.2f}</b>,<br> y: 
+                # x: <b>{self.pos_x:.2f}</b>,<br> y:
+                f'<b>{self.pos_y:.3f}</b>'
             )
 
-    
     def clear_other_display_text(self):
         widgets = self.main_window.graph_widgets.values()
         for widget in widgets:
@@ -648,18 +716,21 @@ class GraphWidget(pg.PlotWidget):
             ox = np.arange(len(oy)) * 0.002
             curve.setData(ox, oy)
 
-    def mouse_click_event(self, ev):
-        if ev.button() == Qt.MouseButton.RightButton:
-            self.context_menu(ev)
-            ev.accept()
+    # def mouse_click_event(self, ev):
+    #     if ev.button() == Qt.MouseButton.RightButton:
+    #         ev.accept()
+    #         self.context_menu(ev)
+    #         return
+    #     return super().mouse_click_event(ev)
 
-    def context_menu(self, ev):
-        menu = QMenu()
-        close_action = QAction('Закрыть (Средняя клавиша мышки)')
-        close_action.triggered.connect(
-            lambda: self.main_window.delete_graph_window(self.graph_names))
-        menu.addAction(close_action)
-        menu.exec(ev.screenPos().toPoint())
+    # def context_menu(self, ev):
+    #     menu = QMenu()
+    #     close_action = QAction('Закрыть (Средняя клавиша мышки)')
+    #     close_action.triggered.connect(
+    #         lambda: self.main_window.delete_graph_window(self.graph_names))
+    #     menu.addAction(close_action)
+    #     menu.exec(ev.screenPos().toPoint())
+
 
 class VidGraph(pg.PlotWidget):
     def __init__(self, main_window, pos=-1):
@@ -677,17 +748,18 @@ class VidGraph(pg.PlotWidget):
         self.setClipToView(True)
         self.setDownsampling(auto=True, mode='peak')
         self.scene().sigMouseClicked.connect(self.mouse_click_event)
-        
+
         pen = pg.mkPen(width=2)
         data = self.main_window.data.get_object(
             'vid_data')
-        
+
         oy = data[self.pos] if len(data) else []
         ox = list(range(len(oy)))
-        curve = pg.PlotDataItem(ox, oy, name='vid_data', pen=pen, connect='all')
+        curve = pg.PlotDataItem(ox, oy, name='vid_data',
+                                pen=pen, connect='all')
         self.addItem(curve)
         self.curve = curve
-    
+
     def mouse_click_event(self, ev):
         if ev.button() == Qt.MouseButton.RightButton:
             self.context_menu(ev)
@@ -730,7 +802,6 @@ class VidGraph(pg.PlotWidget):
         super().closeEvent(ev)
 
 
-
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     app.setStyle('Fusion')
@@ -764,8 +835,7 @@ if __name__ == '__main__':
         }
     '''
 
-    app.setStyleSheet(Stylesheet) 
+    app.setStyleSheet(Stylesheet)
     window = MainWindow(app)
     window.show()
-    sys.exit(app.exec())
-
+    app.exec()
